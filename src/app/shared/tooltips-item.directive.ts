fca133fc4880d426cdaf4215f66c00bd4a0e1ed0
SG\karpowa <y.karpowa@orenpay.ru>
2025-12-03 01:13:21 +0500

    Исправление замечаний ДЗ-4
    Разделены стили, добавлена директива  для события click, исправлен вывод description

--- src/app/shared/tooltips-item.directive.ts
+++ src/app/shared/tooltips-item.directive.ts
@@ -22,14 +22,7 @@ export class TooltipsItemDirective {
     );
     
     // Стили тултипа
-    this.renderer.setStyle(this.tooltipElement, 'position', 'absolute');
-    this.renderer.setStyle(this.tooltipElement, 'background-color', 'lightblue');
-    this.renderer.setStyle(this.tooltipElement, 'color', 'blue');
-    this.renderer.setStyle(this.tooltipElement, 'padding', '4px 8px');
-    this.renderer.setStyle(this.tooltipElement, 'border-radius', '4px');
-    this.renderer.setStyle(this.tooltipElement, 'font-size', '12px');
-    this.renderer.setStyle(this.tooltipElement, 'z-index', '1000');
-    this.renderer.setStyle(this.tooltipElement, 'white-space', 'nowrap');
+    this.renderer.addClass(this.tooltipElement,'app-tooltip');
 
     // Добавляем в body, чтобы не влиять на позиционирование
     this.renderer.appendChild(document.body, this.tooltipElement);
@@ -43,7 +36,7 @@ export class TooltipsItemDirective {
       this.tooltipElement = null;
     };
   }
-  
+
   private positionTooltip() {
     if (!this.tooltipElement) return;
 
@@ -51,9 +44,15 @@ export class TooltipsItemDirective {
     //const tooltipPos = this.tooltipElement.getBoundingClientRect();
 
     const top = hostPos.bottom + window.scrollY + 6;
-    const left = hostPos.left + window.scrollX + hostPos.width;
+    const left = hostPos.left + window.scrollX + hostPos.width/2;
 
     this.renderer.setStyle(this.tooltipElement, 'top', `${top}px`);
     this.renderer.setStyle(this.tooltipElement, 'left', `${left}px`);
+  }
+  
+  ngOnDestroy() {
+    if (this.tooltipElement) {
+      this.renderer.removeChild(document.body, this.tooltipElement);
+    }
   }
 }
--- src/app/to-do-list-item/to-do-list-item.component.ts
+++ src/app/to-do-list-item/to-do-list-item.component.ts
@@ -2,10 +2,11 @@ import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TItems } from '../Interface/Interface';
 import { ButtonComponent } from '../button/button.component';
+import { TooltipsItemDirective } from "../shared/tooltips-item.directive";
 
 @Component({
   selector: 'app-to-do-list-item',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, TooltipsItemDirective],
   templateUrl: './to-do-list-item.component.html',
   styleUrl: './to-do-list-item.component.css',
 })
